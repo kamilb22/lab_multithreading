06a08ab527f18512d546abb9867b561f1869ebba
kamil <222574@edu.p.lodz.pl>
2021-04-30 13:29:17 +0200

    Adding the launchOnePatriotWhenNoticesAScudMissileOneHundredTimes test

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -2,10 +2,12 @@ package edu.iis.mto.multithread;
 
 import static org.mockito.Mockito.verify;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.Executor;
 
 @ExtendWith(MockitoExtension.class)
 class RadarTest {
@@ -13,12 +15,14 @@ class RadarTest {
     @Mock
     private PatriotBattery batteryMock;
 
-    @Test
-    void launchPatriotOnceWhenNoticesAScudMissle() {
-        Radar radar = new Radar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+    @RepeatedTest(100)
+    void launchOnePatriotWhenNoticesAScudMissileOneHundredTimes() {
+        int RocketsToLaunch = 1;
+        Executor executor = (Runnable r) -> r.run();
+        BetterRadar radar = new BetterRadar(RocketsToLaunch, batteryMock, executor);
+        Scud enemyMissile = new Scud();
+        radar.notice(enemyMissile);
+        verify(batteryMock).launchPatriot(enemyMissile);
     }
 
 }
